2416d40446a91cec9d935d3b1052eec82a5acea0
Zachary <zguan999@live.unc.edu>
2020-10-08 01:58:52 -0400

    added views for usage and url routing for usage

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from backend.usage.views import ReportsViewSet
+
+router = routers.DefaultRouter()
+router.register("usage", ReportsViewSet, basename="usage")
 
 urlpatterns = [
+    path('api/',include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
--- backend/usage/views.py
+++ backend/usage/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework import routers, viewsets, status
+from rest_framework.response import Response 
+from backend.usage.models import Reports 
+from backend.usage.serializers import ReportsSerializer
+import datetime
 # Create your views here.
+class ReportsViewSet(viewsets.ModelViewSet):
+    serializer_class = ReportsSerializer
+    
+    
+    def get_queryset(self):
+        queryset = Reports.objects.all() 
+        return queryset    
