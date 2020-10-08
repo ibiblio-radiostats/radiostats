56881b74651363c2b088473fe54e31ad8f624bd7
Zachary <zguan999@live.unc.edu>
2020-10-08 01:18:33 -0400

    created models for users and usage

--- backend/usage/models.py
+++ backend/usage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class AuditStatus(object):
+    PROCESSING = "PROCESSING"
+    PROCESSED = "PROCESSED"
+    APPROVAL = "APPROVAL"
+
+
+class Station(models.Model):
+    station_name = models.CharField(max_length=100,default=models.CASCADE)
+
+class Reports(models.Model):
+    report_dtm = models.DateTimeField(null=True,blank=True)
+    bill_start = models.DateTimeField(null=True,blank=True)
+    bill_end = models.DateTimeField(null=True,blank=True)
+    audit_status = models.CharField(max_length=100,default=AuditStatus.APPROVAL)
+    bill_transit = models.IntegerField(default=0)
+    cost_mult = models.DecimalField(default = 0, decimal_places=4,max_digits =30)
+    sid = models.ForeignKey(Station,on_delete=models.CASCADE)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from backend.usage.models import Station
 
 # Create your models here.
+
+class UserRole(object):
+    ADMIN = "ADMIN"
+    STATION_ADMIN = "STATION_ADMIN"
+    STATION_USER = "STATION_USER"
+
+class User(models.Model):
+    sid = models.ForeignKey(Station,on_delete=models.CASCADE)
+    role = models.CharField(max_length=100,default=UserRole.STATION_USER)
