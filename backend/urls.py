06428421eb676b06824e63b0aee0845db88745fe
Caleb Xu <calebcenter@live.com>
2020-10-13 20:53:45 -0400

    Add swagger UI endpoint for API testing

--- backend/urls.py
+++ backend/urls.py
@@ -14,15 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.conf.urls import url
+from django.urls import include, path
+from rest_framework import permissions, routers
 from backend.usage.views import ReportsViewSet
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
 
 router = routers.DefaultRouter()
 router.register("usage", ReportsViewSet, basename="usage")
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ibiblio Radiostats Backend API",
+      default_version='v1',
+      contact=openapi.Contact(email="cjxu@live.unc.edu"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('api/',include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
