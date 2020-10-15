025888a73b91d0cde82d084508162fa1981029cd
Zachary <zguan999@live.unc.edu>
2020-10-14 23:39:36 -0400

    fixed bug with deleting usage info

--- backend/usage/views.py
+++ backend/usage/views.py
@@ -10,32 +10,31 @@ class ReportsViewSet(viewsets.ModelViewSet):
     serializer_class = ReportsSerializer
 
     def get_queryset(self):
-        if self.request.method == 'GET':
-            queryset = Reports.objects.all()
-            audit_status = self.request.query_params.get("audit_status",None)
-            order_by = self.request.query_params.get("order_by",None)
-            start_dt = self.request.query_params.get("start_dt",None)
-            end_dt = self.request.query_params.get("end_dt",None)
-            approval = self.request.query_params.get("approval",None)
+        queryset = Reports.objects.all()
+        audit_status = self.request.query_params.get("audit_status",None)
+        order_by = self.request.query_params.get("order_by",None)
+        start_dt = self.request.query_params.get("start_dt",None)
+        end_dt = self.request.query_params.get("end_dt",None)
+        approval = self.request.query_params.get("approval",None)
 
 
-            if audit_status is not None:
-                queryset = Reports.objects.filter(audit_status=audit_status)
-            if start_dt is not None and end_dt is not None:
-                queryset = queryset.filter(bill_start__range=(start_dt,end_dt))
-            if approval is not None:
-                queryset = queryset.filter(audit_status='PENDING_APPROVAL')
-            else: 
-                queryset = queryset.exclude(audit_status='PENDING_APPROVAL').exclude(
-                    audit_status='UNUSABLE')
+        if audit_status is not None:
+            queryset = Reports.objects.filter(audit_status=audit_status)
+        if start_dt is not None and end_dt is not None:
+            queryset = queryset.filter(bill_start__range=(start_dt,end_dt))
+        if approval is not None:
+            queryset = queryset.filter(audit_status='PENDING_APPROVAL')
+        else: 
+            queryset = queryset.exclude(audit_status='PENDING_APPROVAL').exclude(
+                audit_status='UNUSABLE')
 
-            if order_by is not None:
-                ###need way to dynamically sort by desc or asce
-                sign = order_by.split(':')[-1]
-                order_sign = '-' if sign == 'desc' else ''
-                order_name = order_by.split(':')[0]
-                queryset = queryset.order_by(order_sign + order_name)
-            return queryset
+        if order_by is not None:
+            ###need way to dynamically sort by desc or asce
+            sign = order_by.split(':')[-1]
+            order_sign = '-' if sign == 'desc' else ''
+            order_name = order_by.split(':')[0]
+            queryset = queryset.order_by(order_sign + order_name)
+        return queryset
 
     def update(self,request, *args, **kwargs):
         pk = kwargs['pk']
