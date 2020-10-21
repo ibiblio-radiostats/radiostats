82b132f0b35154ea8812e6cbf02755ef413f97f4
Zachary <zguan999@live.unc.edu>
2020-10-20 22:49:10 -0400

    committing my new serializer for users

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from backend.users.models import UserInfo
+from django.contrib.auth.models import User 
 
 class UserInfoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,3 +10,8 @@ class UserInfoSerializer(serializers.ModelSerializer):
             "sid",
             "role",
         )
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "username", "email", "userinfo"]
