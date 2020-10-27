552386f12170823a5b6d8a1391a5c25c4a764044
Zachary <zguan999@live.unc.edu>
2020-10-26 22:52:52 -0400

    added test cases and slight changes to usage views

--- backend/backend/usage/tests.py
+++ backend/backend/usage/tests.py
@@ -1,3 +1,175 @@
-from django.test import TestCase
+#from django.test import TestCase
+from rest_framework.test import APITestCase,APIClient,force_authenticate,APIRequestFactory
+from backend.usage.views import ReportViewSet
+from backend.usage.models import Report
+from backend.usage.models import Station
+from backend.users.models import UserInfo
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework.authtoken.models import Token
+from django.urls import reverse
+from django.utils import timezone
+import datetime
+import json 
+from django.contrib import auth 
+
+# Create your tests here.	
+class EventTest(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        station = Station.objects.create(station_name="WCPE")
+        station2 = Station.objects.create(station_name="NPR")
+        admin = User.objects.create_superuser('admin', 'admin@test.com', 'password')
+        radio = User.objects.create_user('user','user@test.com','password')
+        UserInfo.objects.create(
+            user=radio, sid = station, role = 'STATION_USER'
+        )
+        UserInfo.objects.create(
+            user=admin, sid = station2, role = 'STATION_USER'
+        )
+        today = timezone.now().replace(hour=0,minute=0,second=0,microsecond=0)
+        Report.objects.create(
+            report_dtm=today,
+            bill_start = today,
+            bill_end = today,
+            audit_status = 'PROCESSING',
+            bill_transit = 3,
+            cost_mult = 2.3,
+            sid = station,
+        )
+
+        Report.objects.create(
+            report_dtm=today,
+            bill_start = today,
+            bill_end = today,
+            audit_status = 'PROCESSING',
+            bill_transit = 3,
+            cost_mult = 2.3,
+            sid = station2,
+        )
+
+        Report.objects.create(
+            report_dtm=today,
+            bill_start = today,
+            bill_end = today,
+            audit_status = 'PROCESSING',
+            bill_transit = 3,
+            cost_mult = 2.3,
+            sid = station2,
+        )
+        
+        Report.objects.create(
+            report_dtm=today,
+            bill_start = today,
+            bill_end = today,
+            audit_status = 'PENDING_APPROVAL',
+            bill_transit = 3,
+            cost_mult = 2.3,
+            sid = station,
+        )       
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.client = APIClient()
+        self.user = User.objects.get(username='user')
+        self.admin = User.objects.get(username='admin')
+        self.admin.token = Token.objects.create(user=self.admin)
+        self.admin.token.save()
+        self.list_view = ReportViewSet.as_view({"get": "list"})
+        self.sid = Station.objects.get(station_name='NPR')
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    def test_login(self):
+        login_response = self.client.post('/rest-auth/login/',{'username':'admin','password':'password'},format='json')
+        key = login_response.data.get('key')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + key)
+        response = self.client.get('/api/usage/',headers={'Authorization':'Token ' + key})
+        self.assertEqual(response.status_code, 200)
+        self.client.credentials()
+
+    # def test_radioBills(self):
+    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+    #     response = self.client.get('/api/usage/')
+    #     date = datetime.date.today().strftime("%Y-%m-%dT%H:%M:%S%zZ")   
+    #     self.assertEqual(json.loads(response.content),[{"id": 1,
+    #     "report_dtm": date,
+    #     "bill_start": date,
+    #     "bill_end": date,
+    #     "audit_status": "PROCESSING",
+    #     "bill_transit": 3,
+    #     "cost_mult": "2.3000",
+    #     "sid": 1,
+    #     "stations": "WCPE"
+    #     }])
+    #     self.client.credentials()
+
+
+    def test_radioApproval(self):
+        login_response = self.client.post('/rest-auth/login/',{'username':'user','password':'password'},format='json')
+        key = login_response.data.get('key')
+        #self.client.force_login(user=self.user)
+        #self.client.credentials(HTTP_AUTHORIZATION='Token ' + key)
+        response = self.client.get('/api/usage/?approval=test',HTTP_AUTHORIZATION='Token ' + key)
+        #print(response.data)
+        self.assertEquals(0,len(response.data))
+
+    def test_adminBills(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token.key)
+        response = self.client.get('/api/usage/')
+        self.assertEquals(3,len(response.data))
+
+    def test_update(self):
+        login_response = self.client.post('/rest-auth/login/',{'username':'user','password':'password'},format='json')
+        key = login_response.data.get('key')
+        response = self.client.patch('/api/usage/3/?status=PROCESSED&approval=t',HTTP_AUTHORIZATION='Token ' + key)
+
+    # def test_adminApproval(self):
+    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token.key)
+    #     response = self.client.get('/api/usage/?approval=test')
+    #     # print(self.client.session)
+    #     # print(json.dumps(response.data))
+    #     self.assertEquals(200,response.status_code)
+    #     self.assertEquals(1,len(response.data))
+
+
+    
+    # def test_dateRange(self):
+    #     request = self.factory.get(reverse("event-list"))
+    #     force_authenticate(request, user=self.user)
+    #     response = self.list_view(request)
+    #     response.render()
+    #     ids = [fields["name"] for fields in response.data]
+    #     self.assertEquals(["in range", "upper boundary", "lower boundary"], ids)
+
+    # def test_noEvents(self):
+    #     Event.objects.all().delete()
+    #     request = self.factory.get(reverse("event-list"))
+    #     force_authenticate(request, user=self.user)
+    #     response = self.list_view(request)
+    #     response.render()
+    #     self.assertEquals(0, len(response.data))
+
+    # def test_oneEvent(self):
+    #     Event.objects.all().delete()
+    #     location = Location.objects.first()
+    #     Event.objects.create(
+    #         location=location,
+    #         church=self.church,
+    #         name="in range",
+    #         start_dtm=datetime.date.today(),
+    #         end_dtm=datetime.date.today() + datetime.timedelta(1),
+    #     )
+    #     Event.objects.create(
+    #         location=location,
+    #         church=self.church,
+    #         name="in range",
+    #         start_dtm=datetime.date.today() + datetime.timedelta(60),
+    #         end_dtm=datetime.date.today() + datetime.timedelta(1),
+    #     )
+    #     request = self.factory.get(reverse("event-list"))
+    #     force_authenticate(request, user=self.user)
+    #     response = self.list_view(request)
+    #     response.render()
+    #     self.assertEquals(1, len(response.data))
--- backend/backend/usage/views.py
+++ backend/backend/usage/views.py
@@ -13,18 +13,21 @@ class ReportViewSet(viewsets.ModelViewSet):
     serializer_class = ReportSerializer
     
     def get_queryset(self):
-
+        print('enter')
         if self.request.user.is_superuser:
+            print('ewfawef')
             queryset = Report.objects.all()
+            approval = self.request.query_params.get("approval",None)
         else: 
+            print('user')
             id = self.request.user.userinfo.sid
             queryset = Report.objects.filter(sid=id)
+            approval = None
 
         audit_status = self.request.query_params.get("audit_status",None)
         order_by = self.request.query_params.get("order_by",None)
         start_dt = self.request.query_params.get("start_dt",None)
         end_dt = self.request.query_params.get("end_dt",None)
-        approval = self.request.query_params.get("approval",None)
 
 
         if audit_status is not None:
@@ -34,26 +37,31 @@ class ReportViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(bill_start__range=(start_dt,end_dt))
         
         if approval is not None and self.request.user.is_superuser:
+            print('hi')
             queryset = queryset.filter(audit_status='PENDING_APPROVAL')
         else: 
             queryset = queryset.exclude(audit_status='PENDING_APPROVAL').exclude(
-                audit_status='  UNUSABLE')
+                audit_status='UNUSABLE')
 
         if order_by is not None:
-            ###need way to dynamically sort by desc or asce
             sign = order_by.split(':')[-1]
             order_sign = '-' if sign == 'desc' else ''
             order_name = order_by.split(':')[0]
             queryset = queryset.order_by(order_sign + order_name)
         return queryset
 
-    def update(self,request, *args, **kwargs):
-        pk = kwargs['pk']
-        stat = self.request.query_params.get("status",None)
-        if Report.objects.filter(pk=pk) is not None and status is not None:
-            Report.objects.filter(pk=pk).update(audit_status=stat)
-            return Response(status=200)
-        pass
+    def partial_update(self,request, *args, **kwargs):
+        if self.request.user.is_superuser:
+            pk = kwargs['pk']
+            stat = self.request.query_params.get("status",None)
+            if Report.objects.filter(pk=pk).exists() and status is not None:
+                Report.objects.filter(pk=pk).update(audit_status=stat)
+                data = list(Report.objects.filter(pk=pk).values())
+                return Response(data,status=204)
+            else:
+                return Response(status=404)
+        else: 
+            return Response('Not Admin',status=403)
 
 class StationViewSet(viewsets.ModelViewSet):
     serializer_class = StationSerializer
