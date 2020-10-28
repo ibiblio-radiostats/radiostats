e7e6c46efe53d1795722bb96586bac1d6bf126cd
Zachary <zguan999@live.unc.edu>
2020-10-27 22:37:08 -0400

    more test cases for backend

--- backend/backend/usage/tests.py
+++ backend/backend/usage/tests.py
@@ -5,7 +5,6 @@ from backend.usage.models import Report
 from backend.usage.models import Station
 from backend.users.models import UserInfo
 from django.contrib.auth.models import User
-
 from rest_framework.authtoken.models import Token
 from django.urls import reverse
 from django.utils import timezone
@@ -14,21 +13,26 @@ import json
 from django.contrib import auth 
 
 # Create your tests here.	
-class EventTest(APITestCase):
-
+class TestUsage(APITestCase):
     @classmethod
     def setUpTestData(cls):
         station = Station.objects.create(station_name="WCPE")
         station2 = Station.objects.create(station_name="NPR")
         admin = User.objects.create_superuser('admin', 'admin@test.com', 'password')
         radio = User.objects.create_user('user','user@test.com','password')
+        radio2 = User.objects.create_user('test','test@test.com','password')
+
         UserInfo.objects.create(
             user=radio, sid = station, role = 'STATION_USER'
         )
         UserInfo.objects.create(
-            user=admin, sid = station2, role = 'STATION_USER'
+            user=admin, sid = station2, role = 'IBIBLIO_ADMIN'
+        )
+        UserInfo.objects.create(
+            user=radio2, sid = station2, role = 'STATION_USER'
         )
         today = timezone.now().replace(hour=0,minute=0,second=0,microsecond=0)
+        month = today + datetime.timedelta(days=31)
         Report.objects.create(
             report_dtm=today,
             bill_start = today,
@@ -45,7 +49,7 @@ class EventTest(APITestCase):
             bill_end = today,
             audit_status = 'PROCESSING',
             bill_transit = 3,
-            cost_mult = 2.3,
+            cost_mult = 2.4,
             sid = station2,
         )
 
@@ -55,7 +59,7 @@ class EventTest(APITestCase):
             bill_end = today,
             audit_status = 'PROCESSING',
             bill_transit = 3,
-            cost_mult = 2.3,
+            cost_mult = 2.5,
             sid = station2,
         )
         
@@ -67,7 +71,19 @@ class EventTest(APITestCase):
             bill_transit = 3,
             cost_mult = 2.3,
             sid = station,
+        )
+
+        Report.objects.create(
+            report_dtm=today,
+            bill_start = month,
+            bill_end = month,
+            audit_status = 'PROCESSING',
+            bill_transit = 6,
+            cost_mult = 3.5,
+            sid = station2,
         )       
+
+
 
     def setUp(self):
         self.factory = APIRequestFactory()
@@ -80,14 +96,20 @@ class EventTest(APITestCase):
         self.sid = Station.objects.get(station_name='NPR')
         self.token = Token.objects.create(user=self.user)
         self.token.save()
+        self.radio = User.objects.get(username='test')
+        self.radio.token = Token.objects.create(user=self.radio)
+        self.radio.save()
 
     def test_login(self):
         login_response = self.client.post('/rest-auth/login/',{'username':'admin','password':'password'},format='json')
         key = login_response.data.get('key')
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + key)
-        response = self.client.get('/api/usage/',headers={'Authorization':'Token ' + key})
+        response = self.client.get('/api/usage/')
         self.assertEqual(response.status_code, 200)
         self.client.credentials()
+        response = self.client.get('/api/usage/')
+        self.assertEqual(response.status_code, 401)
+
 
     def test_radioBills(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
@@ -115,7 +137,7 @@ class EventTest(APITestCase):
     def test_adminBills(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token.key)
         response = self.client.get('/api/usage/')
-        self.assertEquals(3,len(response.data))
+        self.assertEquals(4,len(response.data))
 
     def test_update(self):
         login_response = self.client.post('/rest-auth/login/',{'username':'admin','password':'password'},format='json')
@@ -128,4 +150,54 @@ class EventTest(APITestCase):
         response = self.client.get('/api/usage/?approval=test')
         self.assertEquals(200,response.status_code)
         self.assertEquals(1,len(response.data))
+
+
+    def test_outRangeFilter(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token.key)
+        response = self.client.get('/api/usage/?start_dt=2090-05-31T15:43:00.000Z&end_dt=2090-06-29T15:43:00.000Z')
+        self.assertEquals(0,len(response.data))
+
+    def test_inRangeFilter(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.radio.token.key)
+        start = datetime.date.today() - datetime.timedelta(days=31) 
+        end = datetime.date.today() + datetime.timedelta(days=1) 
+        response = self.client.get('/api/usage/?start_dt={}&end_dt={}'.format(start.strftime("%Y-%m-%dT%H:%M:%SZ") ,end.strftime("%Y-%m-%dT%H:%M:%SZ")  ))
+        self.assertEquals(2,len(response.data))
+
+    def test_dateFilter(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.radio.token.key)
+        start = datetime.date.today() + datetime.timedelta(days=31) 
+        end = datetime.date.today() + datetime.timedelta(days=40)
+        
+        response = self.client.get('/api/usage/?start_dt={}&end_dt={}'.format(start.strftime("%Y-%m-%dT%H:%M:%SZ") ,end.strftime("%Y-%m-%dT%H:%M:%SZ")))
+        self.assertEquals(1,len(response.data))
+
+
+    def test_sortCost(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token.key)
+        response = self.client.get('/api/usage/?order_by=cost_mult')
+        res_lst = [cost['cost_mult'] for cost in json.loads(response.content)]
+        self.assertEquals(['2.3000','2.4000','2.5000','3.5000'],res_lst)
+        response = self.client.get('/api/usage/?order_by=cost_mult:desc')
+        res_lst = [cost['cost_mult'] for cost in json.loads(response.content)]
+        self.assertEquals(['3.5000','2.5000','2.4000','2.3000'],res_lst)
+
+    def test_audit(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token.key)
+        response = self.client.get('/api/usage/?audit_status=PROCESSING')
+        res_lst = [audit['audit_status'] for audit in json.loads(response.content)]
+        self.assertEquals(['PROCESSING','PROCESSING','PROCESSING','PROCESSING'],res_lst)
+    
+    def test_badRequests(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin.token.key)
+        response = self.client.post('/api/usage/',{})
+        self.assertEquals(405,response.status_code)
+        response = self.client.delete('/api/usage/')
+        self.assertEquals(405,response.status_code)
+        response = self.client.put('/api/usage/')
+        self.assertEquals(405,response.status_code)
+
+
+    
+
 
