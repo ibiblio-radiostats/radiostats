b6229577ffecfff62065c3c7fa4e08cc9d15ddbf
Caleb Xu <calebcenter@live.com>
2020-11-01 22:49:04 -0500

    Adjust deployment start scripts for CI

--- deploy/frontend/start.sh
+++ deploy/frontend/start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-backend_protocol=$(if [ $(yq r /config.yml backend.tls) = true ]; then echo "https"; else echo "http"; fi)
-backend_host=$(yq r /config.yml backend.host)
-backend_port=$(yq r /config.yml backend.port)
+# Find the mounted configuration
+export CONFIG_PATH=${CONFIG_PATH:-/config.yml}
+
+backend_protocol=$(if [ $(yq r ${CONFIG_PATH} backend.tls) = true ]; then echo "https"; else echo "http"; fi)
+backend_host=$(yq r ${CONFIG_PATH} backend.host)
+backend_port=$(yq r ${CONFIG_PATH} backend.port)
 [ ! -z "${backend_port}" ] && backend_port=":${backend_port}"
-backend_path=$(yq r /config.yml backend.path)
+backend_path=$(yq r ${CONFIG_PATH} backend.path)
 
 # Create .env file from the top-level config.yml
 cat > .env <<EOL
@@ -16,5 +19,5 @@ EOL
 mv env-config.js /usr/share/nginx/html/env-config.js
 
 # Set up nginx to use our desired port of choice
-export FRONTEND_PORT=$(yq r /config.yml frontend.port)
+export FRONTEND_PORT=$(yq r ${CONFIG_PATH} frontend.port)
 envsubst "`env | awk -F = '{printf \" \\\\$%s\", $1}'`" < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
