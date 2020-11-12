c77e1fa360ec1fff6e48997e2021ccaacd5c9064
Caleb Xu <calebcenter@live.com>
2020-11-12 13:06:17 -0500

    Enforce nonempty bill_start, bill_end, bill_transit

--- backend/backend/usage/models.py
+++ backend/backend/usage/models.py
@@ -14,9 +14,9 @@ class Station(models.Model):
 
 class Report(models.Model):
     report_dtm = models.DateTimeField(null=True,blank=True)
-    bill_start = models.DateTimeField(null=True,blank=True)
-    bill_end = models.DateTimeField(null=True,blank=True)
+    bill_start = models.DateTimeField()
+    bill_end = models.DateTimeField()
     audit_status = models.CharField(max_length=100,choices=AuditStatus.choices,default=AuditStatus.PENDING_APPROVAL)
-    bill_transit = models.IntegerField(default=0)
+    bill_transit = models.IntegerField()
     cost_mult = models.DecimalField(default = 0, decimal_places=4,max_digits =30)
     sid = models.ForeignKey(Station,related_name='stations',on_delete=models.CASCADE)
