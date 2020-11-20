9eddad4486f81a03fc390c2d047b49e6d424f603
Jonathan Chang <jkchang0@live.unc.edu>
2020-11-19 20:02:00 -0500

    Testing branch for new features and url.

--- frontend/src/functions/Bills.js
+++ frontend/src/functions/Bills.js
@@ -13,7 +13,16 @@ const monthNumToName = ["January", "February", "March",
 export function addKeys(data) {
     // Adding new keys [month], [year], and [cost] to the data.
     for (var i = 0; i < data.length; i++) {
+        // Initializing the date and creating a const date for UTC time.
         var date = new Date(data[i].bill_start);
+        const copyDate = new Date(data[i].bill_start);
+
+        // Setting the correct UTC date.
+        date.setDate(copyDate.getUTCDate());
+        date.setMonth(copyDate.getUTCMonth());
+        date.setFullYear(copyDate.getUTCFullYear());
+
+        // Adding the keys.
         data[i].month = monthNumToName[date.getMonth()];
         data[i].year = date.getFullYear();
         data[i].cost = (data[i].bill_transit * data[i].cost_mult).toFixed(2)
