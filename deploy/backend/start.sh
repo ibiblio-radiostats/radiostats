9c96462f5d1b71151b79e591a8394ea29ee44006
Caleb Xu <calebcenter@live.com>
2021-01-03 18:51:15 -0500

    deploy: make backend logging visible in docker

--- deploy/backend/start.sh
+++ deploy/backend/start.sh
@@ -9,4 +9,4 @@ python manage.py collectstatic --noinput
 # Set up mod_wsgi to use our desired port of choice
 export BACKEND_PORT=$(yq r ${CONFIG_PATH} backend.port)
 
-mod_wsgi-express start-server --port=$BACKEND_PORT --url-alias /static /static/ --application-type module --user django --group django backend.wsgi
+mod_wsgi-express start-server --port=$BACKEND_PORT --url-alias /static /static/ --application-type module --user django --group django backend.wsgi --log-to-terminal
