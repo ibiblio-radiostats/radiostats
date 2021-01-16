56d50027ab9beb67155cfad4b91b5013d17d13aa
Caleb Xu <calebcenter@live.com>
2021-01-16 00:14:30 -0500

    Automate backend admin user creation

--- deploy/backend/start.sh
+++ deploy/backend/start.sh
@@ -6,6 +6,12 @@ export CONFIG_PATH=${CONFIG_PATH:-/config.yml}
 # Finish runtime configuration from mounted config.yml
 python manage.py collectstatic --noinput
 
+# Set up admin user if it doesn't exist
+echo -n "Attempting admin user creation..."
+DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_EMAIL=admin@example.com python manage.py createsuperuser --noinput >/dev/null 2>&1 \
+  && echo " ok" \
+  || echo " already exists"
+
 # Set up mod_wsgi to use our desired port of choice
 export BACKEND_PORT=$(yq r ${CONFIG_PATH} backend.port)
 
